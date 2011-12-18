944374e2b17dab7f4b313d915405caf880205b43
Larry Siden, Westside Consulting LLC <lsiden@westside-consulting.com>
2011-12-18 18:10:50 -0500

    "ie should handle and empty file" passes

--- lib/mailtrap/mailboxes.rb
+++ lib/mailtrap/mailboxes.rb
@@ -1,8 +1,15 @@
-require 'rubygems'
-require 'rspec'
-require File.join(File.dirname(__FILE__), %w[ log_parser ])
+#require 'rubygems'
+#require 'rspec'
+#require File.join(File.dirname(__FILE__), %w[ log_parser ])
 
 class Mailtrap
   class Mailboxes
+
+    def initialize(filename)
+    end
+
+    def accounts
+      return []
+    end
   end
 end
